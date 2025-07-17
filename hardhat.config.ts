fc94350749441e88e086e87575a7e25166e892d3
Ashley Ong <ashley_2461994@hotmail.com>
2025-07-17 23:39:28 +0800

    Fix types

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import '@nomicfoundation/hardhat-verify';
+import '@typechain/hardhat';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
